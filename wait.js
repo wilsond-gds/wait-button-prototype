028f7ad78f548958c7ff4a46b855def169f25c0d
Derren Wilson <101639632+wilsond-gds@users.noreply.github.com>
2022-07-15 11:47:31 +0100

    Add files via upload

---
+++ wait.js
@@ -0,0 +1,29 @@
+
+
+function disableFormSubmit(){
+console.log('need to check what this does');
+}
+
+function submitSpinner() {
+    
+    // currently assumes one form and one button
+    // multiple forms - unlikely?
+    // multiple buttons - apply different styling to the buttons that weren't clicked?
+
+      const actionMessage = "Please wait…";
+
+      var formTarget = document.querySelector('form');
+      var formButtonTarget = document.querySelector('form > button');
+
+      formTarget.addEventListener('submit', function (event) {
+      console.log('clicked');
+      event.preventDefault(); // for testing
+            formButtonTarget.classList.add('button--spinner');
+            formButtonTarget.setAttribute('aria-live', 'polite')
+            formButtonTarget.textContent = actionMessage;
+            formButtonTarget.disabled = true;
+            setInterval(()=>{formButtonTarget.textContent = "Still working…"},6000) // 6 seconds allows voice over to complete reading the text before updating the button
+      })
+    }
+
+    submitSpinner()
